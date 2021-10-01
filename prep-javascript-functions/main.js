2c17912c24d0e4f216c574c0b118a208e982d917
DreSean-Sutton <dreseansutton@gmail.com>
2021-09-30 17:01:34 -0700

    Finished Javascript Functions Exercise.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,65 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+var addTwoNumbersResult = addTwoNumbers(2, 2);
+console.log('addTwoNumbers Excercise:', addTwoNumbersResult);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var convertHoursToMinutesResult = convertHoursToMinutes(2);
+console.log('convertHoursToMinutes excercise:', convertHoursToMinutesResult);
+
+function getGreeting(name) {
+  return name;
+}
+
+var getGreetingResult = getGreeting('Hello World!');
+console.log('getGreeting Excercise:', getGreetingResult);
+
+function addMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+var addMultiplyBy5Result = addMultiplyBy5(10, 5);
+console.log('addMultiplyBy5 Exercise:', addMultiplyBy5Result);
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+
+var multiplyAndDivideBy5Result = multiplyAndDivideBy5(35, 10);
+console.log('multiplyAndDivideBy5 Exercise:', multiplyAndDivideBy5Result);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+var subtractTwoNumbersResult = subtractTwoNumbers(22, 7);
+console.log('subtractTwoNumbers Exercise:', subtractTwoNumbersResult);
+
+function getCircleCircumference(radius) {
+  return 2 * Math.PI * radius;
+}
+
+var getCircleCircumferenceResult = getCircleCircumference(5);
+console.log('getCircleCircumference Exercise:', getCircleCircumferenceResult);
+
+function getFullName(firstName, lastName) {
+  return firstName + lastName;
+}
+
+var getFullNameResult = getFullName('Juan ', 'Ramirez');
+console.log('getFullName exercise:', getFullNameResult);
+
+function cube(number) {
+  return number * 5;
+}
+
+var cubeResult = cube(25);
+console.log('cube Exercise:', cubeResult);
